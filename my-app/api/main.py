d6ca76d4bd7d1de70376c76f5218715ef9916222
julie <julie.lechartier@gmail.com>
2026-03-06 17:41:58 +0100

    simplify Dockerfile by removing multistage build configuration

--- my-app/api/main.py
+++ my-app/api/main.py
@@ -1,10 +1,12 @@
-import mysql. connector import os
-from fastapi import FastAPI from fastapi.middleware.cors import
+import os
+import mysql.connector
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-origins = ["*"]
 
-app. add_middleware(
+origins = ["*"]
+app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
@@ -12,19 +14,20 @@ app. add_middleware(
     allow_headers=["*"]
 )
 
-conn = mysql. connector.connect(
-    database=os-getenv ("MYSQL_DATABASE" ) ,
-    user=os. getenv ("MYSQL_USER"),
-    password=os- getenv("MYSQL_ROOT_PASSWORD"),
+# Connexion MySQL
+conn = mysql.connector.connect(
+    database=os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_ROOT_PASSWORD"),
     port=3306,
-    host=os. getenv("MYSQL_HOST" )
+    host=os.getenv("MYSQL_HOST")
 )
 
-@app.get ("/users")
+@app.get("/users")
 async def get_users():
-    cursor = conn. cursor ()
-    sql_select_Query = "select * from utilisateur"
-    cursor execute(sql_select_Query)
+    cursor = conn.cursor()
+    sql_select_query = "SELECT * FROM utilisateur"
+    cursor.execute(sql_select_query)
     records = cursor.fetchall()
     print("Total number of rows in table: ", cursor.rowcount)
-    return {'utilisateurs': records}
+    return {"utilisateurs": records}
